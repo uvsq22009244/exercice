27e2b50d1d80600c4a55558db890ca63af2762f3
uvsq22009244 <princesse.momo@ens.uvsq.fr>
2021-03-17 22:46:38 +0100

    ajout bon programme sur la fonction ligne_effac

--- exo2.py
+++ exo2.py
@@ -1,18 +1,58 @@
 import tkinter as tk
+import random
 
+HEIGHT = 500
+WIDTH = 500
 
-def start():
-    pass
+def affichage(event):
+    """Lors du clic, création de 2 ligne"""
+    if event.x < WIDTH//2 : 
+        canvas.move(ligne1, -10,0) # on déplace la ligne de -10px vers la gauche suivant la largeur et non la hauteur
+    elif event.x > WIDTH*2//3 :
+        canvas.move(ligne2, 10, 0) # on déplace la ligne de 10px vers la droite suivant la largeur
+    else :
+        canvas.move(ligne1, -10,0)
+        canvas.move(ligne2, 10, 0)
+    canvas.after(180000, affichage)
 
-def creer_balle() :
-    x, y = 300, 200
-    r= 20
-    canvas.create_oval((x))
+creer = 1
+
+def restart_pause(ligne):
+    """Suspend et restore le programme """
+    global creer, ligne1, ligne2
+    id_after = canvas.after(20, lambda: affichage(ligne))
+    if creer:
+        affichage(ligne)
+        bouton.config(text="Restart")
+    else:
+        canvas.after_cancel(id_after)
+        bouton.config(text="Pause")
+    creer = 1 - creer
+
+cpt = 0
+
+def ligne_efface():
+    """On va éffacer les 2 lignes"""
+    global cpt, ligne1, ligne2
+    cpt = 1 - cpt # vaut alternativement 0 et 1
+    if cpt == 0:
+        ligne1 = canvas.create_line((WIDTH//2, 0), (WIDTH//2,HEIGHT), fill= "blue")
+        ligne2 = canvas.create_line((WIDTH*2//3, 0), (WIDTH*2//3,HEIGHT), fill= "red") 
+    else:
+        canvas.delete(ligne1, ligne2)
+    #canvas.after(1000, ligne_efface)
 
 racine = tk.Tk()
-canvas = tk.Canvas(racine, bg="black", width=600, height=400)
-canvas.grid()
-bouton = tk.Button(racine, text="Demarrer", command=start)
-bouton.grid()
+canvas = tk.Canvas(racine, bg="white", width= 500, height= 500)
+ligne1 = canvas.create_line((WIDTH//2, 0), (WIDTH//2,HEIGHT), fill= "blue")
+ligne2 = canvas.create_line((WIDTH*2//3, 0), (WIDTH*2//3,HEIGHT), fill= "red")
+bouton1 = tk.Button(racine, text="Pause", command= restart_pause)
+bouton2 = tk.Button(racine, text= "Effacer", command= ligne_efface)
 
-racine.mainloop
+
+canvas.grid(row=0)
+bouton1.grid(row=1)
+bouton2.grid(row=2)
+canvas.bind("<Button-1>", affichage)
+
+racine.mainloop()
